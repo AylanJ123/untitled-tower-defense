3db320eccb9ef69db16039a156c56d1cf40ffcde
AylanJ123 <aylanjosue@gmail.com>
2024-01-27 01:21:22 -0600

    hotfix namespace afflictions

--- Assets/Code/Afflictions/Afflictions.cs
+++ Assets/Code/Afflictions/Afflictions.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace com.vintagerockets.untitledtowerdefense
+namespace com.vintagerockets.untitledtowerdefense.afflictions
 {
     public class Afflictions : MonoBehaviour
     {
